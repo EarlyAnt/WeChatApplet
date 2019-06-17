47eeddea15963afcd30fe917db999a03baae0fe0
EarlyAnt <54763755@qq.com>
2019-06-18 07:34:22 +0800

    1.新闻区域使用数据绑定。 2.修改几个图片文件的名字。(后缀名要全小写，文件名也不能用中文)

---
+++ BoQinHome/pages/data/pictureData.js
@@ -0,0 +1,25 @@
+var datas = [{
+    userPortrait: "../images/girl.png",
+    userName: "qinqin",
+    date: "2019/06/17",
+    newsTitle: "岁月不老，一世倾城",
+    newsImage: "../images/scenery3.jpg",
+    newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
+    likeCount: 32,
+    commentCount: 16
+  },
+  {
+    userPortrait: "../images/boy.png",
+    userName: "bobo",
+    date: "2019/06/18",
+    newsTitle: "岁月不老，一世倾城",
+    newsImage: "../images/scenery5.jpg",
+    newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
+    likeCount: 37,
+    commentCount: 12
+  }
+]
+
+module.exports = {
+  datas: datas
+}
