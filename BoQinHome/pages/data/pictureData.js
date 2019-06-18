2bcfc1da1b73255c363490a91fa39086bddb3763
EarlyAnt <54763755@qq.com>
2019-06-18 22:09:40 +0800

    四个人的主题图片要不一样。

--- BoQinHome/pages/data/pictureData.js
+++ BoQinHome/pages/data/pictureData.js
@@ -23,7 +23,7 @@ var datas = [{
     userName: "xuanxuan",
     date: "2011/01/09",
     newsTitle: "岁月不老，一世倾城",
-    newsImage: "../images/scenery5.jpg",
+    newsImage: "../images/scenery2.jpg",
     newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
     likeCount: 37,
     commentCount: 12
@@ -33,7 +33,7 @@ var datas = [{
     userName: "lele",
     date: "2018/09/20",
     newsTitle: "岁月不老，一世倾城",
-    newsImage: "../images/scenery5.jpg",
+    newsImage: "../images/scenery1.jpg",
     newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
     likeCount: 37,
     commentCount: 12
