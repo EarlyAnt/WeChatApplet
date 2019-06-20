659ccbfcb42416cea334e099746a9842da4a5352
EarlyAnt <54763755@qq.com>
2019-06-21 07:35:00 +0800

    实现了详情页的功能。

--- BoQinHome/pages/data/pictureData.js
+++ BoQinHome/pages/data/pictureData.js
@@ -1,42 +1,46 @@
 var datas = [{
-    userPortrait: "../images/girl.png",
-    userName: "qinqin",
-    date: "1983/03/15",
-    newsTitle: "岁月不老，一世倾城",
-    newsImage: "../images/scenery3.jpg",
-    newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
-    likeCount: 32,
-    commentCount: 16
+    "id": 0,
+    "userPortrait": "../images/girl.png",
+    "userName": "qinqin",
+    "date": "1983/03/15",
+    "newsTitle": "岁月不老，一世倾城",
+    "newsImage": "../images/scenery3.jpg",
+    "newsText": "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
+    "likeCount": 32,
+    "commentCount": 16
   },
   {
-    userPortrait: "../images/boy.png",
-    userName: "bobo",
-    date: "1982/05/03",
-    newsTitle: "岁月不老，一世倾城",
-    newsImage: "../images/scenery5.jpg",
-    newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
-    likeCount: 37,
-    commentCount: 12
+    "id": 1,
+    "userPortrait": "../images/boy.png",
+    "userName": "bobo",
+    "date": "1982/05/03",
+    "newsTitle": "岁月不老，一世倾城",
+    "newsImage": "../images/scenery5.jpg",
+    "newsText": "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
+    "likeCount": 37,
+    "commentCount": 12
   },
   {
-    userPortrait: "../images/girl.png",
-    userName: "xuanxuan",
-    date: "2011/01/09",
-    newsTitle: "岁月不老，一世倾城",
-    newsImage: "../images/scenery2.jpg",
-    newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
-    likeCount: 37,
-    commentCount: 12
+    "id": 2,
+    "userPortrait": "../images/girl.png",
+    "userName": "xuanxuan",
+    "date": "2011/01/09",
+    "newsTitle": "岁月不老，一世倾城",
+    "newsImage": "../images/scenery2.jpg",
+    "newsText": "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
+    "likeCount": 37,
+    "commentCount": 12
   },
   {
-    userPortrait: "../images/boy.png",
-    userName: "lele",
-    date: "2018/09/20",
-    newsTitle: "岁月不老，一世倾城",
-    newsImage: "../images/scenery1.jpg",
-    newsText: "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
-    likeCount: 37,
-    commentCount: 12
+    "id": 3,
+    "userPortrait": "../images/boy.png",
+    "userName": "lele",
+    "date": "2018/09/20",
+    "newsTitle": "岁月不老，一世倾城",
+    "newsImage": "../images/scenery1.jpg",
+    "newsText": "天地悠悠过客匆匆潮起又潮落，恩恩怨怨生死白头几人能看透，红尘啊滚滚痴痴啊情深，聚散终有时，留一半清醒留一半醉，至少梦里有你追随，我拿青春赌明天，你用真情换此生，岁月不知人间多少的忧伤，何不潇洒走一回。",
+    "likeCount": 37,
+    "commentCount": 12
   }
 ]
 
--- BoQinHome/pages/pictureDetail/pictureDetail.js
+++ BoQinHome/pages/pictureDetail/pictureDetail.js
@@ -1,4 +1,4 @@
-// pages/picture.js
+// pages/pictureDetail/pictureDetail.js
 var pageData = require("../data/pictureData.js");
 
 Page({
@@ -7,76 +7,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-    indicatorDots: true,
-    autoPlay: true,
-    interval: 2000,
-    circular: true,
     useData: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
+    console.log(options.id);
     this.setData({
-      useData: pageData.datas
+      useData: pageData.datas[options.id]
     });
+    console.log(useData);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
-  },
-
-  transfer: function(event) {
-    console.log(event);
-    console.log(event.currentTarget.dataset.id);
-    wx.navigateTo({
-      url: '../pictureDetail/pictureDetail?id='+event.currentTarget.dataset.id
-    })
   }
 })
